c827f4168cd0150d138c17a246a45a36130f78a5
Doum <dominic.lapointe@exova.com>
2018-05-29 16:57:57 -0400

    start ts

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="q-app">
+    <router-view />
+  </div>
+</template>
+
+<script lang='ts'>
+import Vue from "vue";
+
+export default class App extends Vue {
+  name: string = "App";
+}
+
+// export default {
+//   name: "App"
+// };
+</script>
+
+<style>
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    <button v-on:click="say()">something from Tss</button>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script lang='ts'>
+import Vue from "vue";
+
+export default class Index extends Vue {
+  public name: string = "index";
+
+  private say = (): void => {
+    console.log(this.name);
+  };
+}
+</script>
