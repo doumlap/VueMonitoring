8080916721a51ef1fa8ad5825065ee08b0278c77
Doum <dominic.lapointe@exova.com>
2018-06-15 17:58:06 -0400

    start dashboard playground

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
   <q-layout view="hHr LpR lFf">
     <q-layout-header>
-      <q-toolbar color="primary" :glossy="$q.theme === 'mat'" :inverted="$q.theme === 'mat'">
+      <q-toolbar color="secondary" :glossy="true">
         <q-btn flat dense round aria-label="Menu" @click="mini=!mini">
           <q-icon name="menu"></q-icon>
         </q-btn>
         <q-toolbar-title>
-          <router-link to="/">Monitoring</router-link>
+          <router-link style="cursor: pointer;" tag="div" to="/">Monitoring</router-link>
           <div slot="subtitle">Running on Quasar v{{ $q.version }}</div>
         </q-toolbar-title>
       </q-toolbar>
@@ -15,9 +15,11 @@
     <q-layout-drawer :mini="mini" side="left" v-model="drawerOpen">
       <q-list separator>
         <q-item to="/" link exact>
+          <q-item-side color="primary" left icon="dashboard"><q-tooltip anchor="top right">Dashboard</q-tooltip></q-item-side>
           <q-item-main label="Dashboard"></q-item-main>
         </q-item>
         <q-item to="/setup" link exact>
+        <q-item-side color="primary" left icon="room"><q-tooltip anchor="top right">Setup</q-tooltip></q-item-side>
           <q-item-main label="Setup"></q-item-main>
         </q-item>
       </q-list>
@@ -34,11 +36,12 @@ import Vue from "vue";
 import Component from "vue-class-component";
 
 @Component({ name: "DefaultLayout" })
-export default class DefaultLayout extends Vue { 
+export default class DefaultLayout extends Vue {
   private drawerOpen: boolean = true;
   private mini: boolean = true;
 }
 </script>
 
-<style>
+<style lang="stylus">
+@import '~variables';
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,21 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page padding class="flex content-start">
+	  <div class="flex-container">
+		  <q-toggle class="q-pr-sm col" padding v-model="isOnlyError" color="negative" label="Only Error" left-label />
+		  <q-toggle class="q-pr-sm col" padding v-model="isOnlyWarning" color="warning" label="Only Warning" left-label />
+		  <q-toggle class="q-pr-sm col" padding v-model="isOnlySuccess" color="positive" label="Only Success" left-label />
+	  </div>
+	  <div class="flex-container">
+		  <q-card class="q-ma-sm col" inline v-for="task in activeTasks" v-bind:key="task.name">
+			<q-card-title v-bind:class="{'bg-negative': task.status=='error', 'bg-positive': task.status=='success', 'bg-warning': task.status=='warning'}">
+				{{task.name}}
+			</q-card-title>
+			<q-card-separator />
+			<q-card-main>
+				{{task.msg}}
+			</q-card-main>
+		  </q-card>
+	  </div>
   </q-page>
 </template>
 
@@ -13,10 +28,90 @@ import Component from "vue-class-component";
 
 @Component({ name: "Index" })
 export default class Index extends Vue {
+  private tasks: Array<any> = new Array<any>();
+  private isOnlyError: boolean = false;
+  private isOnlyWarning: boolean = false;
+  private isOnlySuccess: boolean = false;
+
   constructor() {
     super();
 
-    console.log('index');
+    console.log("index");
+  }
+
+  get activeTasks() {
+    if (this.isOnlyError === true) {
+      return this.tasks.filter(t => t.status === "error");
+    } else if (this.isOnlyWarning === true) {
+      return this.tasks.filter(t => t.status === "warning");
+    } else if (this.isOnlySuccess === true) {
+      return this.tasks.filter(t => t.status === "success");
+    } else {
+      return this.tasks;
+    }
+  }
+
+  mounted() {
+    this.tasks.push({
+      name: "Task 1",
+      status: "error",
+      msg: "Cannot retrieve server X"
+    });
+    this.tasks.push({
+      name: "Task 2",
+      status: "success",
+      msg: "Cannot retrieve server X"
+    });
+    this.tasks.push({
+      name: "Task 3",
+      status: "warning",
+      msg: "Cannot retrieve server X"
+    });
+    this.tasks.push({
+      name: "Task 4",
+      status: "error",
+      msg: "Cannot retrieve server X"
+    });
+    this.tasks.push({
+      name: "Task 5",
+      status: "error",
+      msg: "Cannot retrieve server X"
+    });
+    this.tasks.push({
+      name: "Task 6",
+      status: "error",
+      msg: "Cannot retrieve server X"
+    });
+    this.tasks.push({
+      name: "Task 7",
+      status: "error",
+      msg: "Cannot retrieve server X"
+    });
+    this.tasks.push({
+      name: "Task 8",
+      status: "error",
+      msg: "Cannot retrieve server X"
+    });
+    this.tasks.push({
+      name: "Task 9",
+      status: "error",
+      msg: "Cannot retrieve server X"
+    });
+    this.tasks.push({
+      name: "Task 10",
+      status: "error",
+      msg: "Cannot retrieve server X"
+    });
+    this.tasks.push({
+      name: "Task 11",
+      status: "error",
+      msg: "Cannot retrieve server X"
+    });
+    this.tasks.push({
+      name: "Task 12",
+      status: "error",
+      msg: "Cannot retrieve server X"
+    });
   }
 }
 </script>
